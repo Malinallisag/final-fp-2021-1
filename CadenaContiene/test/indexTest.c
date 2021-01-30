f8e691f18e0e6bd5e48c8d5cad073974e41117a7
Malinallisag <72509737+Malinallisag@users.noreply.github.com>
2021-01-29 18:03:50 -0600

    Update indexTest.c

--- CadenaContiene/test/indexTest.c
+++ CadenaContiene/test/indexTest.c
@@ -1,7 +1,12 @@
-char cadena1[] = "gentrificacion";
-char cadena2[] = "gente";
-buscar(cadena1, cadena2), 0
-
+#include "..\index.c"
+int main(void)
+{
 char cadena1[] = "almacenes";
 char cadena2[] = "alma";
-buscar(cadena1, cadena2), 1
+if (buscar(cadena1, cadena2), 1)
+{
+printf("\t\n Existe %s dentro de %s\n", cadena2, cadena1);
+} else {
+printf("\t\n No existe %s dentro de %s\n", cadena2, cadena1);
+}
+}
